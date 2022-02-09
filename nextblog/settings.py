33edb74ca8b7f0f8f24ad13c56026aa4d9af6488
Ragib Ajmal <ragibajmal@Ragib-Ajmal.local>
2022-02-09 22:17:41 +0530

    added rest jwt

--- nextblog/settings.py
+++ nextblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vqp1n34&#q*g%e6a!_(%!8zypf9$4l**$t#b)*jh9hp!)f5y_i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['infinite-eyrie-81096.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['infinite-eyrie-81096.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -38,17 +38,35 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 ROOT_URLCONF = 'nextblog.urls'
 
--- nextblog/urls.py
+++ nextblog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainSlidingView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('auth.urls')),
+    path('token-auth/', TokenObtainSlidingView.as_view()),
 ]
